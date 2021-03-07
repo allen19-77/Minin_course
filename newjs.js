924224c5101580df6c1f1cbe4539584a28db0b6a
allen19-77 <allen.19@list.ru>
2021-03-07 09:17:01 +0500

    newjs

---
+++ newjs.js
@@ -0,0 +1,28 @@
+let divs = document.querySelectorAll('div')
+let link = document.querySelector('a')
+
+
+for (let i = 0; i < divs.length; i++) {
+	divs[i].addEventListener('click', function (event) {
+		event.stopPropagation()
+		console.log(this.getAttribute('id'))
+	})
+}
+
+link.addEventListener('click', handleLinkClick)
+
+function handleLinkClick(event) {
+	event.preventDefault()
+	let div = divs[0]
+	/*if (div.style.display === 'none') {
+		div.style.display = 'flex'
+	} else {
+		div.style.display = 'none'
+	}*/
+
+	div.style.display = div.style.display === 'none'
+		? 'flex'
+		: 'none'
+
+	console.log(div.style.display)
+}
